f2c998f780cf3fb8f46a17c2bf704aaba919e9a9
Jonathan Leon <jonathan_leonl@hotmail.com>
2022-01-26 16:28:52 -0600

    api documentos usuarios

--- documentacion.php
+++ documentacion.php
@@ -11,12 +11,21 @@
   $response = [
     'data' => [],
     'message' => '',
-    'method' => $method
+    'method' => $method,
+    'status' => 'error'
   ];
 
   // OBTENER DOCUMENTOS
   if ( 'GET' == $method && isset($_GET['id']) && !empty($_GET['id']) ) {
-    // TODO listar los documentos del usuario
+    $sql = "SELECT Consecutivo,TipoDocumento, UrlDocumento, FechaAlta
+      FROM mg_ctedoctos WHERE NumeroCliente = {$params->id} ORDER BY TipoDocumento ";
+
+    $rows = mysqli_query($conn ,$sql);
+    while( $reg = mysqli_fetch_assoc($rows) ) {
+        array_push($response['data'], $reg);
+    }
+
+    $response['status'] = 'ok';
   }
 
   // GUARDAR DOCUMENTO NUEVO
@@ -30,12 +39,30 @@
     $newFileName = $_FILES['file']['name'];
     $fileTmpPath = $_FILES['file']['tmp_name'];
     $uploadFileDir = './documentacion/';
-    $data = $_POST['data']; // json con la info
+    $data = json_decode($_POST['data']);
 
     $dest_path = $uploadFileDir . $newFileName;
 
     if ( move_uploaded_file($fileTmpPath, $dest_path) ) {
       $response['message'] = 'File is successfully uploaded.';
+      $registro = mysqli_query($conn ,
+        "CALL mgsp_ClientesDocumentos(
+          '$data->userId',
+          '$data->idDoc',
+          '$data->consDoc',
+          '$dest_path',
+          NOW(),
+          @OutErrorClave,
+          @OutErrorProcedure,
+          @OutErrorDescripcion)"
+      );
+      $row = mysqli_query(
+        $conn,
+        "SELECT @OutErrorClave AS errorClave, @OutErrorProcedure AS errorSp, @OutErrorDescripcion AS errorDescripcion"
+      );
+      while( $reg = mysqli_fetch_assoc($row) ) {
+        array_push($response['data'], $reg);
+      }
     } else {
       $response['message'] = 'There was some error moving the file to upload directory. Please make sure the upload directory is writable by web server.';
     }
@@ -45,6 +72,12 @@
   // ELIMINAR DOCUMENTO
   if ( 'DELETE' === $method && $_POST['id'] ) {
     // TODO falta meter la eliminacion del archivo.
+    $registro = mysqli_query($conn ,"CALL mgsp_ClientesDocumentosBorrar('$params->userId','$params->consDocumento',@OutErrorClave,@OutErrorProcedure,@OutErrorDescripcion)");
+    $row = mysqli_query($con,"SELECT @OutErrorClave as errorClave,@OutErrorProcedure as errorSp,@OutErrorDescripcion as errorDescripcion");
+    while( $reg = mysqli_fetch_assoc($row) ) {
+      array_push($response['data'], $reg);
+    }
+    $response['status'] = 'ok';
   }
 
   echo json_encode($response);
