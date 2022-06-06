8dda91e706d0e4937c3bd68a99863722cb8ae099
Jonathan Leon <jonathan_leonl@hotmail.com>
2022-06-06 13:01:22 -0500

    credenciales cloud db

--- conexion.php
+++ conexion.php
@@ -1,5 +1,11 @@
 <?php
-function returnConection(){
-    $con = mysqli_connect("127.0.0.1","root","","firmex2");
+function returnConection() {
+    $con = mysqli_connect("firmex.mysql.database.azure.com","firmex_admin","qaz123WSX345","firmex");
+    if (!$con) {
+        echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
+        echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
+        echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
+        exit;
+    }
     return $con;
 }
